47eb6f6baf346282a420461997b44df672d4b7c6
Mathieu Binette <mbinette91@gmail.com>
2015-01-17 15:20:47 -0500

    Added partial for book list. Can be used on / and /search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.order(created_at: :desc)
   end
 
   def search
