7cfeae149361232430ccc3abe14c7922bbd27937
Mathieu Binette <mbinette91@gmail.com>
2015-01-17 13:19:09 -0500

    Added seeding stuff necessary to seed courses. Added Course controller to list courses.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'courses/index'
+
+  get 'course/index'
+
   resources :books
+
+  resources :courses
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
