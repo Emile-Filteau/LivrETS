71418cbb0f27fabe7c684d9f9da32017e6c8d6f3
Émile Filteau <emile.filteau@gmail.com>
2015-01-17 13:31:03 -0500

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
-  
+
   resources :courses
 
   # The priority is based upon order of creation: first created -> highest priority.
