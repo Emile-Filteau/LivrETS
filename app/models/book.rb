390660b08b4b451cb98aa8b7323a2bf4e55839c6
Émile Filteau <emile.filteau@gmail.com>
2015-01-17 19:37:18 -0500

    Validations pour la création de livres et css

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,9 @@ class Book < ActiveRecord::Base
   has_and_belongs_to_many :courses
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "250x250>" }, :default_url => "/images/:style/missing_book.svg"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+  validates :email, presence: true, format: {with: /A.+@.+Z/}
+  validates :name, presence: true, length: {minimum: 5}
+  validates :state, presence: true, numericality: { only_integer: true, greater_than:0, less_than_or_equal_to:5 }
+  validates :contact_name, presence: true
+  validates :price, presence: true, numericality: {greater_than: 0}
 end
